f081bdebec353a40be5903129924ee7a7874f9ba
Ta-syn Islam Nishan <140194155+ta-syn@users.noreply.github.com>
2026-05-04 23:36:31 +0600

    Fix: Change X-Frame-Options to SAMEORIGIN for Vercel preview

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
           },
           {
             key: 'X-Frame-Options',
-            value: 'DENY',
+            value: 'SAMEORIGIN',
           },
           {
             key: 'X-XSS-Protection',
